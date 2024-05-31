1f327e102bca6048c4c53c7122e82bdb00a05c24
Asako Hayase <chipmunk.kck@gmail.com>
2024-05-30 21:24:37 -0600

    feat: Display avatars for active users

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Live from "@/components/Live";
+import Navbar from "@/components/Navbar";
 
 export default function Page() {
   return (
-    <div className="">
+    <main className="h-screen overflow-hidden">
+      <Navbar />
       <Live />
-    </div>
+    </main>
   );
 }
